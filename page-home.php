0cdeac582c6e6bc8cd132e3469017758214693de
Alex Horton <alexhortonmusic@gmail.com>
2017-04-24 11:42:25 -0500

    setup homepage sass

--- page-home.php
+++ page-home.php
@@ -9,6 +9,15 @@
 get_header();
 ?>
 
-<h1>HOME PAGE</h1>
+<div class="homepage">
+  <div class="idx-video-wrap">
+    <div class="idx-box">
+
+    </div>
+    <div class="video-box">
+
+    </div>
+  </div>
+</div>
 
 <?php get_footer(); ?>
