7f69aeb57ec510d16609cf1d0aebabfd9ebc7985
Alex Horton <alexhortonmusic@gmail.com>
2017-04-24 15:39:39 -0500

    added widget area/made iframe responsive

--- functions.php
+++ functions.php
@@ -7,4 +7,18 @@
     }
     add_action( 'wp_enqueue_scripts', 'wpb_adding_scripts' );
 
+    if (function_exists('register_sidebar')) {
+
+    	register_sidebar(array(
+    		'name' => 'Widgetized Area',
+    		'id'   => 'widgetized-area',
+    		'description'   => 'This is a widgetized area.',
+    		'before_widget' => '<div id="%1$s" class="widget %2$s">',
+    		'after_widget'  => '</div>',
+    		'before_title'  => '<h4>',
+    		'after_title'   => '</h4>'
+    	));
+
+    }
+
  ?>
--- page-home.php
+++ page-home.php
@@ -10,12 +10,19 @@ get_header();
 ?>
 
 <div class="homepage">
+
   <div class="idx-video-wrap">
     <div class="idx-box">
 
-    </div>
-    <div class="video-box">
+    	<?php if (function_exists('dynamic_sidebar') && dynamic_sidebar('widgetized-area')) : else : ?>
 
+    	<?php endif; ?>
+
+    </div>
+    <div class="video-box-wrap">
+      <div class="video-box">
+        <iframe src="https://player.vimeo.com/video/148142207" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+      </div>
     </div>
   </div>
 </div>
