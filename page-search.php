2be508037c600b91fa50fcb33c1b388d58c28004
Alex Horton <alexhortonmusic@gmail.com>
2017-04-26 17:45:40 -0500

    single and multi posts page

--- page-search.php
+++ page-search.php
@@ -12,4 +12,18 @@ get_header();
 <h1>SEARCH PAGE</h1>
 <p>Advanced search, automatic updates on new listings</p>
 
+<div class="searchpage">
+  <?php if (function_exists('dynamic_sidebar') && dynamic_sidebar('search_page')) : else : ?>
+
+  <?php endif; ?>
+
+  <div class="email-alerts">
+    <a href="http://localhost/dwell-local/email-alerts/">
+      <div class="alerts-btn" style="border: 1px solid red; background-color: red; color: white; height: 30px; width: 150px; margin: 0 auto;">
+        Get email alerts based on your search
+      </div>
+    </a>
+  </div>
+</div>
+
 <?php get_footer(); ?>
--- page-sell.php
+++ page-sell.php
@@ -13,4 +13,8 @@ get_header();
 <p>PDF of roadmap</p>
 <p>info blurb, Call to action to contact an agent to sell</p>
 
+<img src="http://localhost/dwell-local/wp-content/uploads/2017/04/dwell_seller_roadmap.jpg" alt="Seller Roadmap PDF">
+
+<!-- make link to open the pdf ____ http://localhost/dwell-local/wp-content/uploads/2017/04/dwell_seller_roadmap.pdf -->
+
 <?php get_footer(); ?>
